7e22c6de6478a408b5c1ff7c4abff07caa2e9b2a
srinurmaharanii18 <119285115+srinurmaharanii18@users.noreply.github.com>
2022-11-28 15:04:11 +0700

    Add files via upload

---
+++ if.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main(){
+    int nilai =78;
+    if (nilai > 78){
+        cout << "lulus";
+   } else if (nilai == 78){
+        cout << "lulus kkm";
+   } else {
+        cout << "tidak lulus ";
+}
+return 0;
+}
