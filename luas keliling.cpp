5fab1a65bd78d01a44eabc4239e94578f55356b0
srinurmaharanii18 <119285115+srinurmaharanii18@users.noreply.github.com>
2022-11-28 15:16:31 +0700

    Add files via upload

---
+++ luas keliling.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int jari2, pi;
+  int luas1, luas2, keliling1, keliling2
+  cout << "NAMA : Sri Nurmaharani" << endl;
+  cout << "Kelas: X PPLG 2"<< endl;
+  jari2 = 15;
+  pi = 22/7;
+  luas1 = pi * jari2;
+  luas2 = luas1 * jari2
+  cout << "L = pi * jari-jari" << endl;
+  cout << " = " ≤< pi << " * " << jari2 << endl;
+  cout << " = " << luas1 << " * " << jari2 <<endl;
+  cout << " = " << luas2 << endl;
+  keliling1 = 2 * pi;
+  keliling2 = keliling * jari2;
+  cout << *k = 2 * pi * jari-jari" << endl;
+  cout << " = 2 * " << pi << " * " << jari2 << endl;
+  cout << " = " << keliling1 << " * " << jari2 << endl;
+  cout << " = " << keliling2 << endl;
+  }
